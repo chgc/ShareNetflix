c4a965e25ee09f3b4a013b4df7e4bcd5ee554602
Kevin Yang <chgc_kevin@hotmail.com>
2018-04-16 17:27:36 +0800

    加入 firebase ，完成基本分享功能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
 import { EventPageService } from './event-page.service';
-import { PopupComponent } from './popup/popup.component';
 import { MaterialModule } from './material.module';
+import { PopupComponent } from './popup/popup.component';
 import { TimePipe } from './time.pipe';
 
 @NgModule({
@@ -16,8 +18,10 @@ import { TimePipe } from './time.pipe';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     AppRoutingModule
   ],
   providers: [EventPageService],
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,16 +1,20 @@
-import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { EventPageService } from '../event-page.service';
+import { Observable } from 'rxjs/Observable';
+import { Video } from '@models/video';
+import { Comment } from '@models/comment';
 
 const mockData = {
-  bgImages: 'https://occ-0-1077-2219.1.nflxso.net/art/85c06/94902a1fbfac64496b110ce690fab014e8185c06.webp',
-  episode: 11,
   id: '80178687',
+  title: '黑閃電',
+  summary: '家人遭到一幫歹徒威脅後，學校校長兼收山的超級英雄傑弗森·皮爾斯再次迅速行動，化身為傳奇人物黑閃電。',
+  bgImages: 'https://occ-0-1077-2219.1.nflxso.net/art/85c06/94902a1fbfac64496b110ce690fab014e8185c06.webp',
   releaseYear: 2018,
-  runtime: 8100,
+  episode: 11,
   session: 1,
   numSeasonsLabel: '1 季',
-  summary: '家人遭到一幫歹徒威脅後，學校校長兼收山的超級英雄傑弗森·皮爾斯再次迅速行動，化身為傳奇人物黑閃電。',
-  title: '黑閃電'
+  runtime: 8100
 };
 
 @Component({
@@ -20,12 +24,30 @@ const mockData = {
 })
 export class PopupComponent implements OnInit {
   video = mockData;
+  comment = '';
+  private itemsCollection: AngularFirestoreCollection<Video>;
+  private commentsCollection: AngularFirestoreCollection<Comment>;
 
-  constructor(private eventPageService: EventPageService, private cd: ChangeDetectorRef) {}
+  constructor(private eventPageService: EventPageService, private cd: ChangeDetectorRef, private db: AngularFirestore) {
+    this.itemsCollection = this.db.collection<Video>('lists');
+    this.commentsCollection = this.db.collection<Comment>('comments');
+  }
 
   ngOnInit() {
     this.addListner();
     this.requestVideoInfo();
+  }
+
+  share() {
+    const postData = { ...this.video, updateDate: new Date() };
+    this.itemsCollection.doc(postData.id).set(postData, { merge: true });
+    this.commentsCollection
+      .doc(postData.id)
+      .collection('comments')
+      .add({ id: this.db.createId(), comment: this.comment })
+      .then(() => {
+        this.comment = '';
+      });
   }
 
   private addListner() {
