c55cacb50da20b02222de872a4d83d45208cd133
Kevin Yang <chgc_kevin@hotmail.com>
2018-04-30 12:05:57 +0800

    add load more function

--- src/app/sites/app.component.ts
+++ src/app/sites/app.component.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, KeyValueDiffers, KeyValueDiffer } from '@angular/core';
 import { Video, GenresDisplay } from '@models/video';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, mergeMap, shareReplay, tap, switchMap } from 'rxjs/operators';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { map, mergeMap, shareReplay, tap, switchMap, scan } from 'rxjs/operators';
 import * as firebase from 'firebase/app';
 import memo from 'memo-decorator';
 
@@ -18,12 +18,28 @@ export class AppComponent implements OnInit {
     .get('assets/data.json')
     .pipe(map(data => data['genres']), shareReplay());
   triggerSearch$ = new BehaviorSubject([]);
+  searchDiffer: KeyValueDiffer<any, any>;
 
+  private query = {
+    limit: 9
+  };
+  private data = new BehaviorSubject({ data: [], needReset: false });
+  private more = new BehaviorSubject(false);
   private genresSet = new Set();
-  constructor(private db: AngularFirestore, private http: HttpClient) { }
+  constructor(private differs: KeyValueDiffers, private db: AngularFirestore, private http: HttpClient) {}
 
   ngOnInit() {
+    this.searchDiffer = this.differs.find(this.genres).create();
     this.initDataset();
+    this.videos$ = this.data.pipe(
+      scan((acc: Video[], value: { data; needReset }) => {
+        if (value.needReset) {
+          return [...value.data];
+        }
+        return [...acc, ...value.data];
+      }, []),
+      shareReplay()
+    );
   }
 
   trackByFn(index, item: Video) {
@@ -44,36 +60,63 @@ export class AppComponent implements OnInit {
     return Array.from(this.genresSet);
   }
 
+  get loadMoreDisable() {
+    return this.data.value.data.length < this.query.limit;
+  }
+
+  loadMore() {
+    this.more.next(true);
+  }
+
   private initDataset() {
-    this.videos$ = this.triggerSearch$.pipe(
-      switchMap((filters) => {
-        return this.db.collection<Video>('videos', ref => {
-          let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
-          if (filters) {
-            filters.forEach(genre => {
-              query = query.where(`genres.${genre.id}`, "==", true);
+    let needReset = false;
+    let cursor = null;
+    combineLatest(this.triggerSearch$, this.more)
+      .pipe(
+        switchMap(([filters, more]) => {
+          return this.db
+            .collection<Video>('videos', ref => {
+              let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+              needReset = this.searchDiffer.diff(filters) !== null ? true : false;
+
+              filters.forEach(genre => {
+                query = query.where(`genres.${genre.id}`, '==', true);
+              });
+              if (filters.length === 0) {
+                query = query.orderBy('likes', 'desc');
+              }
+
+              query = query.limit(this.query.limit);
+              if (!needReset && more) {
+                query = query.startAfter(cursor);
+              }
+              return query;
             })
-          } else {
-            query = query.orderBy('likes', 'desc')
-          }
-          return query;
-        }).valueChanges();
-      }),
-      mergeMap(videos => {
-        return this.genres$.pipe(
-          map(genres =>
-            videos.map(video => ({
-              ...video,
-              genresDisplay: Object.keys(video.genres).map(id => this.getGenresDisplay(id, genres)).filter(x => x)
-            }))
-          )
-        );
-      }),
-      map(videos => videos.sort((a: Video, b: Video) =>
-        a.likes > b.likes ? -1 : a.likes < b.likes ? 1 : 0
-      )),
-      shareReplay()
-    );
+            .snapshotChanges();
+        }),
+        mergeMap(videos => {
+          return this.genres$.pipe(
+            map(genres =>
+              videos.map(video => {
+                cursor = video.payload.doc;
+                return {
+                  ...video.payload.doc.data(),
+                  genresDisplay: Object.keys(video.payload.doc.data().genres)
+                    .map(id => this.getGenresDisplay(id, genres))
+                    .filter(x => x)
+                };
+              })
+            )
+          );
+        }),
+        map(videos => videos.sort((a: Video, b: Video) => (a.likes > b.likes ? -1 : a.likes < b.likes ? 1 : 0))),
+        tap(videos => {
+          cursor = videos.length === 0 ? null : cursor;
+          this.data.next({ data: videos, needReset });
+          needReset = false;
+        })
+      )
+      .subscribe();
   }
 
   @memo()
